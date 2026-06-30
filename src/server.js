668e7ebc17f6c9d7bb18db80ca0944589bcf320f
Ricardo Henrique Lins de Jesus <ricardohenriquelins@outlook.com.br>
2026-06-30 11:08:15 -0300

    fix: server.js agora usa app.js corretamente

--- src/server.js
+++ src/server.js
@@ -1,26 +1,7 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import app from './app.js'
 
-const app = express();
-
-// 🔥 necessário para ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.json());
-
-// 🔥 serve arquivos do frontend (PASTA PUBLIC NA RAIZ)
-app.use(express.static(path.join(__dirname, '../public')));
-
-// 🔥 rota principal
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
-
-// 🔥 porta do Railway
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
